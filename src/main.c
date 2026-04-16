0fe2b4e95aa07191bcd8a0f2e1893a19c85d9450
pastraman <pastraman1@gmail.com>
2026-04-16 11:33:10 +0300

    implement main

--- src/main.c
+++ src/main.c
@@ -1,6 +1,31 @@
+#include "codegen.h"
+#include "lexer.h"
+#include "parser.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main() {
-  printf("Hello, World!\n");
+int main(int argc, char *argv[]) {
+  FILE *file = fopen(argv[1], "r");
+  fseek(file, 0, SEEK_END);
+  long size = ftell(file);
+  fseek(file, 0, SEEK_SET);
+  char *source = malloc(size + 1);
+  fread(source, 1, size, file);
+  source[size] = '\0';
+  fclose(file);
+
+  int count;
+  Token *tokens = tokenize(source, &count);
+  chip chips = parse(tokens, count);
+
+  char outName[256];
+  strcpy(outName, argv[1]);
+  char *dot = strrchr(outName, '.');
+  strcpy(dot, ".v");
+
+  generate(outName, chips);
+
+  free(source);
   return 0;
 }
